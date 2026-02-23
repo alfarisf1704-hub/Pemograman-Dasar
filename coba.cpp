d76ee507121cb59f3a7296fc974ce0d31a94fc2d
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-02-23 15:29:34 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,12 @@
+// contoh library
+#include <iostream>
+using namespace std;
+
+// variabel global
+string nama;
+
+int main()
+{            // program utama
+    int age; // variable local
+    cout << "hello world" << endl;
+}
